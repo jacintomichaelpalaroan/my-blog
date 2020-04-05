bcb0e6b20d8373af7dc13816075c7d3fe050a8e4
JacintoMichaelPalaroan <palaroan.jacintomichael@gmail.com>
2020-04-05 09:24:39 +0800

    initial commit

---
+++ src/build/precache-manifest.edeece17149c464bfcaafb4caa395f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e702501236e71852d164e8e25d481545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd89524edca748f59cba",
+    "url": "/static/css/main.975566ef.chunk.css"
+  },
+  {
+    "revision": "dd7be130b9e1edf48178",
+    "url": "/static/js/2.ac4f3662.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ac4f3662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd89524edca748f59cba",
+    "url": "/static/js/main.1fa9f0e7.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
